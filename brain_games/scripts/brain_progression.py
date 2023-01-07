1cd37701230f5f268e45078bf5e1102285944c5c
ibrg <romanbezh@gmail.com>
2023-01-07 02:06:55 +0200

    create game brain-prograsion

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,41 @@
+from random import randint
+from brain_games.cli import welcome, welcome_user, getPlayerAnswer, getPlayerName, compareAnswers
+
+
+def game(player_name):
+
+    counter = 3
+
+    while counter > 0:
+        lst = [x for x in range(randint(1, 100))]
+        start_index = randint(0, 25)
+        progres_num = randint(1, 8)
+        lst = lst[start_index::progres_num]
+        index = randint(1, len(lst)-1)
+        hide_num = lst[index]
+        lst[index] = '..'
+        
+        print("Question: ", end='')
+        print(*lst)
+
+        answer = getPlayerAnswer()
+        check_answer = compareAnswers(hide_num, answer, player_name)
+        if not check_answer:
+            break
+        print(check_answer)
+        counter -= 1
+    else:
+        print(f"Congratulations, { player_name }!")
+
+
+def main():
+    welcome()
+    game_rule = 'What number is missing in the progression?'
+    player_name = getPlayerName()
+    welcome_user(game_rule, player_name)
+    game(player_name)
+
+
+
+if __name__ == '__main__':
+    main()
